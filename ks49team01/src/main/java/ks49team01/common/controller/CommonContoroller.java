b2f08b4e3a7de160307c179a4120922bfd064204
ksmart <ksmart@DESKTOP-LIJ9H19>
2024-01-15 17:01:39 +0900

    [ks49team01 - SHJ] index화면 추가 detail: index화면 추가 및 내용 추가

---
+++ ks49team01/src/main/java/ks49team01/common/controller/CommonContoroller.java
@@ -0,0 +1,18 @@
+package ks49team01.common.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class CommonContoroller {
+	
+	@GetMapping(value = {"","/"})
+	public String index() {
+		
+		return "common/index";
+	}
+	
+
+}
