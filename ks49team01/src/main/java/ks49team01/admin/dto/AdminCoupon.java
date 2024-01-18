0510897eb5040cb0b2138aba8a581a03fff1f467
ASUS <ASUS@DESKTOP-T50VO8O>
2024-01-18 10:35:34 +0900

    [ks49team01] 리뷰,쿠폰,마일리지관리 details : getCouponKind dto연결

--- ks49team01/src/main/java/ks49team01/admin/dto/AdminCoupon.java
+++ ks49team01/src/main/java/ks49team01/admin/dto/AdminCoupon.java
@@ -1,5 +1,15 @@
 package ks49team01.admin.dto;
 
-public class AdminCoupon {
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+@Data
+public class AdminCoupon {
+	private String couponCode;
+	private String memberId;
+	private String couponName;
+	private String couponPrice;
+	private String couponDueDate;
+	private String couponKindRegisterDate;
 }
