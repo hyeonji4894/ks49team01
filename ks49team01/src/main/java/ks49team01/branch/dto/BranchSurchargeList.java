113daf4eb2eb962042ce84224aff0207a97cfa7b
ksmart <ksmart@DESKTOP-LIJ9H19>
2024-01-19 17:24:41 +0900

    [ks49team01 - SHJ] 객실관리 detail: branch 연결시작

--- ks49team01/src/main/java/ks49team01/branch/dto/BranchSurchargeList.java
+++ ks49team01/src/main/java/ks49team01/branch/dto/BranchSurchargeList.java
@@ -1,5 +1,20 @@
 package ks49team01.branch.dto;
 
-public class BranchSurchargeList {
+import lombok.Data;
 
+@Data
+public class BranchSurchargeList {
+	// 리스트 등록에 필요
+	private String seasonSurchargeListCode;
+	private String listRegistrantId;
+	private String lowseasonPeakseason;
+	private String dayOfTheWeek;
+	private String systemRegisterDate;
+	
+	// 가맹점이 등록한 리스트 조회에 필요하여 추가
+	private String branchSeasonUnitPriceCode;
+	private String surchargeRegistrantId;
+	private String branchCode;
+	private String additionalAmount;
+	private String registerDate;
 }
