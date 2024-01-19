f88dde26afb56a3355a365420ae260444925b08c
ASUS <ASUS@DESKTOP-T50VO8O>
2024-01-19 17:27:00 +0900

    [ks49team01 - KDW] 쿠폰, 마일리지 관리 details : 가맹점 쿠폰 화면 데이터베이스 연결

--- ks49team01/src/main/java/ks49team01/branch/mapper/BranchCouponMapper.java
+++ ks49team01/src/main/java/ks49team01/branch/mapper/BranchCouponMapper.java
@@ -1,5 +1,15 @@
 package ks49team01.branch.mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import ks49team01.branch.dto.BranchCoupon;
+
+@Mapper
 public interface BranchCouponMapper {
 
+	List<BranchCoupon> getCouponList();
+	
+	List<BranchCoupon> getSendCouponList();
 }
--- ks49team01/src/main/java/ks49team01/branch/service/BranchCouponService.java
+++ ks49team01/src/main/java/ks49team01/branch/service/BranchCouponService.java
@@ -1,5 +1,37 @@
 package ks49team01.branch.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import ks49team01.branch.dto.BranchCoupon;
+import ks49team01.branch.mapper.BranchCouponMapper;
+import lombok.AllArgsConstructor;
+
+@Service
+@Transactional
+@AllArgsConstructor
 public class BranchCouponService {
 
+	private BranchCouponMapper branchCouponMapper;
+	
+	/**
+	 * 쿠폰 종류 조회
+	 */
+	
+	public List<BranchCoupon> getCouponList(){
+		
+		return branchCouponMapper.getCouponList();
+	}
+	
+	/**
+	 * 쿠폰 지급 목록 조회
+	 */
+	
+	public List<BranchCoupon> getSendCouponList(){
+		
+		return branchCouponMapper.getSendCouponList();
+		
+	}
 }
