03e937b43c711e353205a441c6ba6c5893153fdd
sinhyeonji <sinhyeonji@192.168.123.100>
2024-01-29 22:43:10 +0900

    [ks49team01 - SHJ] user 공통 detail : user메인 화면 수

--- ks49team01/src/main/java/ks49team01/admin/controller/MainController.java
+++ ks49team01/src/main/java/ks49team01/admin/controller/MainController.java
@@ -1,24 +1,17 @@
 package ks49team01.admin.controller;
 
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import ks49team01.admin.dto.AdminRoom;
-import ks49team01.admin.service.AdminRoomService;
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 @AllArgsConstructor
-@Slf4j
 @Controller("/adminMainController")
 @RequestMapping("/admin")
 public class MainController {
 	
-	private final AdminRoomService roomService;
+	//private final AdminRoomService roomService;
 
 	@GetMapping(value={"","/"})
 	public String mainPage() {
@@ -26,18 +19,17 @@ public class MainController {
 		return "admin/main";
 	}
 	
-	@GetMapping("/roomInfo")
-	public String roomInfo(Model model) {
-	
-	List<AdminRoom> roomInfoList = roomService.getRoomInfoList();
-	
-	log.info("객실목록: {}", roomInfoList);
-	
-	model.addAttribute("service", "객실정보");
-	model.addAttribute("serviceUri", "/admin/room");
-	model.addAttribute("pageTitle", "객실정보조회");
-	model.addAttribute("roomInfoList", roomInfoList);
-	
-	return "admin/main";
-}
+	/*
+	 * @GetMapping("/roomInfo") public String roomInfo(Model model) {
+	 * 
+	 * List<AdminRoom> roomInfoList = roomService.getRoomInfoList();
+	 * 
+	 * log.info("객실목록: {}", roomInfoList);
+	 * 
+	 * model.addAttribute("service", "객실정보"); model.addAttribute("serviceUri",
+	 * "/admin/room"); model.addAttribute("pageTitle", "객실정보조회");
+	 * model.addAttribute("roomInfoList", roomInfoList);
+	 * 
+	 * return "admin/main"; }
+	 */
 }
