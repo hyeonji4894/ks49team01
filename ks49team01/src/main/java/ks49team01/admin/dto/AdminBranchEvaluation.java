385575c71d68c81dded2697661eeb6f07a41bccb
ksmart <ksmart@DESKTOP-LIJ9H19>
2024-01-29 14:02:23 +0900

    [ks49team01] : admin xml.html파일 수정 detail : admin xml.html파일 수정

--- ks49team01/src/main/java/ks49team01/admin/dto/AdminBranchEvaluation.java
+++ ks49team01/src/main/java/ks49team01/admin/dto/AdminBranchEvaluation.java
@@ -9,25 +9,25 @@ public class AdminBranchEvaluation {
 	private String memberId;
 	private String branchEvaluationRegisterDate;
 	private String listOfBranchEvaluationQuestions;
-	private String grading_code;
-	private String branch_recruit_code;
-	private int applicant_response;
-	private int score_after_grading;
-	private String grading_criteria_code;
+	private String gradingCode;
+	private String branchRecruitCode;
+	private int applicantResponse;
+	private int scoreAfterGrading;
+	private String gradingCriteriaCode;
 	private String standard;
 	private String more;
 	private String under;
-	private int score_num;
+	private int scoreNum;
 	private int score;
-	private String evaluation_criteria_date;
-	private String pass_or_not_code;
-	private int evaluation_total_score;
-	private String pass_or_not;
-	private String total_evaluation_code;
-	private String close_school_code;
+	private String evaluationCriteriaDate;
+	private String passOrNotCode;
+	private int evaluationTotalScore;
+	private String passOrNot;
+	private String totalEvaluationCode;
+	private String closeSchoolCode;
 	private int total;
-	private String registrant_id;
-	private String system_registration_time;
+	private String registrantId;
+	private String systemRegistrationTime;
 	
 }	
 
