15d99423e31b8d2d7f33bd0b18417770131f530b
ksmart <ksmart@DESKTOP-LIJ9H19>
2024-01-30 17:00:23 +0900

    [ks49team01 - SHJ] 객실관리 detail: user 객실 리스트 수정, user mail 페이지 수정

--- ks49team01/src/main/java/ks49team01/user/controller/MainController.java
+++ ks49team01/src/main/java/ks49team01/user/controller/MainController.java
@@ -1,16 +1,33 @@
 package ks49team01.user.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import ks49team01.user.dto.UserRoom;
+import ks49team01.user.service.UserRoomService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@AllArgsConstructor
 @Controller("/userMainController")
 @RequestMapping("/user")
 public class MainController {
 
+	private final UserRoomService roomService;
+	
 	@GetMapping(value={"","/"})
-	public String mainPage() {
+	public String mainPage(Model model) {
 		
+		List<UserRoom> roomList = roomService.getRoomList("");
+		log.info("roomList:{}", roomList);
+		if(roomList != null) roomList = roomList.stream().limit(8).toList();
+		model.addAttribute("roomList", roomList);
 		return "user/main";
 	}
+	
 }
