d453fdcb50b19feb074c3a9bba9a450280883b12
ksmart <ksmart@DESKTOP-LIJ9H19>
2024-02-08 15:27:16 +0900

    [ks49team01 - SHJ] 객실관리, 메인페이지 detail : 메인 페이지 객실 클릭시 일치하는 객실페이지로 이동 설정, 메인페이지 account -myPage 화면 연결 및 화면 생성, footer수정, header 메뉴 중 사용하지 않는 메뉴 삭제, user객실 자세히 보기 화면에서 예약하기 클릭시 옵션 선택화면으로 이동할 수 있도록 경로 수정

--- ks49team01/src/main/java/ks49team01/user/controller/MainController.java
+++ ks49team01/src/main/java/ks49team01/user/controller/MainController.java
@@ -6,8 +6,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import ks49team01.user.dto.UserRoom;
+import ks49team01.user.mapper.UserRoomMapper;
 import ks49team01.user.service.UserRoomService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,15 +21,31 @@ import lombok.extern.slf4j.Slf4j;
 public class MainController {
 
 	private final UserRoomService roomService;
+	private final UserRoomMapper roomMapper;
 	
 	@GetMapping(value={"","/"})
-	public String mainPage(Model model) {
+	public String mainPage(Model model,@RequestParam(value = "location", required = false)
+							String location) {
 		
-		List<UserRoom> roomList = roomService.getRoomList("");
+		List<String> locationList = roomMapper.getLocationList();
+		
+		List<UserRoom> roomList = roomService.getRoomList(location);
 		log.info("roomList:{}", roomList);
 		if(roomList != null) roomList = roomList.stream().limit(8).toList();
 		model.addAttribute("roomList", roomList);
+		
+		model.addAttribute("roomList",roomList);
+		model.addAttribute("locationList", locationList);
+		
 		return "user/main";
 	}
+	
+	@GetMapping("/account/myPage")
+	public String myPage(Model model) {
+		
+		model.addAttribute("pageTitle", "마이페이지");
+		return "user/account/my_page";
+	}
+	
 	
 }
